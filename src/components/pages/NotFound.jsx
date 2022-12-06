4c41b84df3ec7a7e1dc2945df93a9db7338fe99e
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 16:56:04 +0700

    update

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <div>
+            NotFound.<Link to="/">Go back home</Link>
+        </div>
+    )
 }
 
 export default NotFound
