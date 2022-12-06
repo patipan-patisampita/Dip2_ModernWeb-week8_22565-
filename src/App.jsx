f947a7d2d093ea8ef0bdb15bde529b476df21e8c
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 15:34:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import RootLayout from "./components/layouts/RootLayout"
-import Counter from "./components/pages/Counter"
+import Home from "./components/pages/Home"
 
 function App() {
   console.log("process.env.PORT: ", process.env.PORT)
   return (
     <>
       <RootLayout>
-        <Counter />
+        <Home />
       </RootLayout>
     </>
   )
