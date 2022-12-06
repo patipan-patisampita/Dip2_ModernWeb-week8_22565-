db9af4f4445782271796a77b21127aa884eea4d5
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 17:04:13 +0700

    update

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
     return (
         <div className='d-flex justify-content-center align-items-center'>
-            NotFound.<Link to="/">Go back home</Link>
+            <p>NotFound.<Link to="/">Go back home</Link></p>
         </div>
     )
 }
