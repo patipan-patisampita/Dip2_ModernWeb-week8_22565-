358f2b750dc7085182f5fae142b6dd2e695bb0c7
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 15:51:52 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react"
+import { Route, Routes } from "react-router-dom"
 import RootLayout from "./components/layouts/RootLayout"
+import About from "./components/pages/About"
 import Home from "./components/pages/Home"
+import Profile from "./components/partials/Profile"
 
 function App() {
   console.log("process.env.PORT: ", process.env.PORT)
   return (
     <>
       <RootLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
         <Home />
       </RootLayout>
     </>
--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
       <nav className="navbar navbar-expand-sm navbar-dark shadow sticky-top" style={{ backgroundColor: "#A708E8" }}>
         <div className="container">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <Link className="navbar-brand" to="/">Navbar</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">About</a>
+                <Link className="nav-link active" to="/about">About</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">Profile</a>
+                <Link className="nav-link active" to="/profile">Profile</Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link active" href="#">Fruit</a>
