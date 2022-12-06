1044212b42aa1d97e1d475f32d97dce0e84a0820
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-06 16:18:56 +0700

    update

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa";
+import { FaSlideshare } from "react-icons/fa";
+import { FaCalendarPlus } from "react-icons/fa";
+import { FaLeaf } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -13,16 +17,16 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/"><FaHome /> Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" to="/about">About</Link>
+                <Link className="nav-link active" to="/about"><FaSlideshare /> About</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" to="/profile">Profile</Link>
+                <Link className="nav-link active" to="/profile"><FaCalendarPlus /> Profile</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">Fruit</a>
+                <a className="nav-link active" href="#"><FaLeaf /> Fruit</a>
               </li>
             </ul>
           </div>
